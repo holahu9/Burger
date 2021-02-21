7df7ee11841bde4b794bc2172853386d1a6c21ba
Duc Dinh <holahu9@gmail.com>
2021-02-21 15:12:00 -0500

    update

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Grill Chicken Sanwich', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger King', false);
